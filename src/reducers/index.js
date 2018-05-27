52111f57543c0bac70c3f36367a3a3d7b034f542
Quan Truong <qmtruong92@gmail.com>
2018-05-26 21:43:33 -0700

    Assigned the PostReducer in rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
-
+import PostsReducer from '../reducer_post';
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+
 });
 
 export default rootReducer;
