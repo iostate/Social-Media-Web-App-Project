28de7e202bd31b288cd705ca0ae6b01775248463
Quan Truong <qmtruong92@gmail.com>
2018-05-29 19:42:58 -0700

    Fixed react router issue with Switch

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import promise from 'redux-promise';
-import {BrowserRouter, Route } from 'react-router-dom';
+import {BrowserRouter, Route, Switch } from 'react-router-dom';
 
 
 import reducers from './reducers';
@@ -16,9 +16,11 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-      
+      <Switch>
+        <Route path="/posts/new" component={PostsNew} />    
         <Route path="/" component={PostsIndex} />
-        <Route path="/posts/new" component={PostsNew} />           
+              
+      </Switch>
       </div>
     </BrowserRouter>
   </Provider>
