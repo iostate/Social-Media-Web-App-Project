b9b1273ac86570d38c83c14ccb97c37dd6436813
Quan Truong <qmtruong92@gmail.com>
2018-05-29 19:38:21 -0700

    Render the new post page. Caveat: React Router still shows up the base url

---
+++ src/components/posts_new.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class PostsNew extends Component {
+
+	render() {
+		return (
+			<div>
+				<h3>PostsNew!</h3>
+			</div>
+		);
+	}
+}
+
+export default PostsNew;
--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import {BrowserRouter, Route } from 'react-router-dom';
 
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -15,19 +16,9 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-      { 
-        // What does this do? 
-        // It's the base URL. Whenever a user fucks up, bring them here. It renders all posts.  
+      
         <Route path="/" component={PostsIndex} />
-
-        // What does this do? 
-        // It's a URL for a specific post. It renders a specific post based on an ID
-        // <Route path="/posts/:id" component={PostsShow} />
-
-        // What does this do?
-        // It's the New Post URL. It renders a form that allows user to create a new post
-        // <Rotue path="/posts/new" component={PostsNew} />
-      }
+        <Route path="/posts/new" component={PostsNew} />           
       </div>
     </BrowserRouter>
   </Provider>
