894df91234391b90f86db7da81f4f61f4624240b
Quan Truong <qmtruong92@gmail.com>
2018-06-02 04:46:55 -0700

    + Create post action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,34 @@
 import axios from 'axios';
+import { func } from 'prop-types';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=PAPERCLIP2134';
 
+/**
+ * Fetches all the posts from the API.
+ */
 export function fetchPosts() {
 	// payload for all the posts on the API
 	const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 	console.log(request);
 	return {
 		type: FETCH_POSTS,
+		payload: request
+	};
+}
+
+/**
+ * Post a new post to the API. 
+ * @param {*} values 
+ */
+export function createPost(values) {
+	const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values);
+
+	return {
+		type: CREATE_POST,
 		payload: request
 	};
 }
