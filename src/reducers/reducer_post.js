ac2f5b2ca9fce8061571d69896d8de4800881245
Quan Truong <qmtruong92@gmail.com>
2018-06-02 06:03:12 -0700

    state[post.id] = post

--- src/reducers/reducer_post.js
+++ src/reducers/reducer_post.js
@@ -1,9 +1,18 @@
 import _ from 'lodash';
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST } from '../actions';
 
 // default state as an object
 export default function(state = {}, action) {
 	switch(action.type) {
+		case FETCH_POST:
+			// const post = action.payload.data;
+			// const newState = { ...state };
+			// newState[post.id] = post; // take newState, give it the index of the post.id and set it equal to post
+			// return newState;
+			// need to make sure that the new state has the id of the post as a key
+
+			// get the id of the post, access state[id] = post 
+			return {...state, [action.payload.data.id]:action.payload.data};
 		case FETCH_POSTS:
 			return _.mapKeys(action.payload.data, "id");
 		default:
