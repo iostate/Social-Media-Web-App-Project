6296ddf8806f7eb948e9d2d4922127d09fa58fb3
Quan Truong <qmtruong92@gmail.com>
2018-06-02 09:09:35 -0700

    + Delete Post Button, helper function, deletePost action creator

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { fetchPost } from '../actions';
+import { fetchPost, deletePost } from '../actions';
 
 class PostsShow extends Component {
 	componentDidMount() {
 		// gets the ID out of the URL
 		const { id } = this.props.match.params;
 		this.props.fetchPost(id);
+	}
+
+	onDeleteClick() {
+		// gets the ID out of the URL
+		const { id } = this.props.match.params;
+		this.props.deletePost(id, () => {
+			this.props.history.push('/');
+		});
 	}
 
 	render() {
@@ -23,6 +31,12 @@ class PostsShow extends Component {
 		return (
 			<div>
 				<Link to="/" className="btn btn-primary">Back to Index</Link>
+				<button
+					className="btn btn-danger pull-xs-right"
+					onClick={this.onDeleteClick.bind(this)}
+				>
+					Delete Post
+				</button>
 				<h3>{post.title}</h3>
 				<h6>Categories: { post.categories }</h6>
 				<p>{post.content}</p>
@@ -42,4 +56,4 @@ function mapStateToProps({posts}, ownProps) {
 	return {post: posts[ownProps.match.params.id]};
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
