4fd652237c156d2b6ff3b9be5aed2ab42de83f2d
Quan Truong <qmtruong92@gmail.com>
2018-06-02 05:02:33 -0700

    + Callback parameter added to createPost in order to navigate user back to PostsIndex

--- src/actions/index.js
+++ src/actions/index.js
@@ -21,11 +21,15 @@ export function fetchPosts() {
 }
 
 /**
- * Post a new post to the API. 
- * @param {*} values 
+ * Post a new post to the API. After successfully posting,
+ * execute the callback function. 
+ * @param {*} values Object containing the values of the form. 
+ * @param {*} callback A function that navigates the user back to 
+ * PostsIndex.
  */
-export function createPost(values) {
-	const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values);
+export function createPost(values, callback) {
+	const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values)
+		.then(() => callback());
 
 	return {
 		type: CREATE_POST,
