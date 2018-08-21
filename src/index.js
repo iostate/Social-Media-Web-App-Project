fe4f46748927832e1cd157790cc18609b99efeb5
Quan Truong <qmtruong92@gmail.com>
2018-08-21 14:38:40 -0700

    Removed the messenger route

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
 import promise from 'redux-promise';
-import {BrowserRouter, Route, Switch } from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import reducers from './reducers';
 
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
 import PostsShow from './components/posts_show';
+import MessagingHeader from './components/messaging/MessagingHeader';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -16,12 +17,14 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-      <Switch>
-        <Route path="/posts/new" component={PostsNew} />
-        <Route path="/posts/:id" component={PostsShow} />
-        <Route path="/" component={PostsIndex} />
-      </Switch>
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/posts/:id" component={PostsShow} />
+          {/* <Route path="/messenger" component={MessagingHeader} /> */}
+          <Route path="/" component={PostsIndex} />
+        </Switch>
       </div>
     </BrowserRouter>
-  </Provider>
-  , document.querySelector('.container'));
+  </Provider>,
+  document.querySelector('.container')
+);
